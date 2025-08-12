2f255e5770434f24c5e5610461144049bc5f3bf2
Jordan Davis <jdavis.software@gmail.com>
2025-08-12 13:53:56 -0700

    Adds stories from src directory to Storybook
    Extends Storybook's stories to include components defined
    within the `src` directory, improving component coverage and
    discoverability in the Storybook environment.

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.tsx'],
+  stories: ['../stories/**/*.stories.tsx', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
